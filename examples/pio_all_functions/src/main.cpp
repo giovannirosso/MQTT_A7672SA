9bbc2000a516869652e0d0a645cdf4e16982a5be
Giovanni Rosso <giovanni_unruh@hotmail.com>
2024-05-30 09:29:33 -0300

    --

--- examples/pio_all_functions/src/main.cpp
+++ examples/pio_all_functions/src/main.cpp
@@ -3,9 +3,10 @@
 
 A7672SA modem(GPIO_NUM_17, GPIO_NUM_16, GPIO_NUM_27);
 
-#define APN "virtueyes.com.br"
+// #define APN "virtueyes.com.br"
 // #define APN "inlog.claro.com.br"
-// #define APN "nbiot.gsim"
+#define APN "nbiot.gsim"
+// #define APN "fulltime.com.br"
 
 #define SSL
 
@@ -30,9 +31,11 @@ void mqttCallback(mqtt_message &message)
     }
     printf("\n");
 
+    byte payload[message.length + 1];
+    memcpy(payload, message.payload, message.length);
     if (strcmp(message.topic, "teste/sub") == 0)
     {
-        modem.mqtt_publish("teste/pub", "OK", strlen("OK"), 0, 5000);
+        modem.mqtt_publish("teste/pub", payload, message.length, 0, 5000);
     }
 }
 
@@ -40,9 +43,9 @@ void setup()
 {
     modem.begin();
 
-    modem.on_message_callback(mqttCallback);
+    // modem.on_message_callback(mqttCallback);
 
-    modem.on_mqtt_status(mqttStatusCallback);
+    // modem.on_mqtt_status(mqttStatusCallback);
 
 start:
 
@@ -54,69 +57,71 @@ start:
     bool sim = modem.sim_ready(5000);
     ESP_LOGI("SIM", "%d", sim);
 
-    int signal;
-    uint32_t start = millis();
-    do
-    {
-        signal = modem.signal_quality(5000);
-        ESP_LOGI("SIGNAL", "%d", signal);
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-    } while (signal == 99 && millis() - start < 2 * 60000);
-    if (signal == 99)
-    {
-        modem.restart();
-    }
+    // int signal;
+    // uint32_t start = millis();
+    // do
+    // {
+    //     signal = modem.signal_quality(5000);
+    //     ESP_LOGI("SIGNAL", "%d", signal);
+    //     vTaskDelay(2000 / portTICK_PERIOD_MS);
+    // } while (signal == 99 && millis() - start < 2 * 60000);
+    // if (signal == 99)
+    // {
+    //     modem.restart();
+    // }
 
-    bool apn = modem.set_apn(APN, 5000);
-    ESP_LOGI("APN", "%d", apn);
-    start = millis();
-    while (!modem.wait_network() && millis() - start < 10000)
-    {
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-    }
-    if (millis() - start >= 10000)
-    {
-        modem.restart();
-    }
+    // bool apn = modem.set_apn(APN, 5000);
+    // ESP_LOGI("APN", "%d", apn);
+    // start = millis();
+    // while (!modem.wait_network() && millis() - start < 10000)
+    // {
+    //     vTaskDelay(5000 / portTICK_PERIOD_MS);
+    // }
+    // if (millis() - start >= 10000)
+    // {
+    //     modem.restart();
+    // }
 
-    bool ntp = modem.set_ntp_server("pool.ntp.org", 0, 5000);
-    ESP_LOGI("NTP", "%d", ntp);
+    // bool ntp = modem.set_ntp_server("pool.ntp.org", 0, 5000);
+    // ESP_LOGI("NTP", "%d", ntp);
 
-    String provider;
-    do
-    {
-        provider = modem.get_provider_name(5000);
-        printf("PROVIDER: %s\n", provider);
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-    } while (provider == "");
+    // String provider;
+    // do
+    // {
+    //     provider = modem.get_provider_name(5000);
+    //     printf("PROVIDER: %s\n", provider);
+    //     vTaskDelay(2000 / portTICK_PERIOD_MS);
+    // } while (provider == "");
 
-    String imei;
-    do
-    {
-        imei = modem.get_imei(5000);
-        printf("IMEI: %s\n", imei.c_str());
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-    } while (imei == "");
+    // String imei;
+    // do
+    // {
+    //     imei = modem.get_imei(5000);
+    //     printf("IMEI: %s\n", imei.c_str());
+    //     vTaskDelay(2000 / portTICK_PERIOD_MS);
+    // } while (imei == "");
 
-    String ip;
-    do
-    {
-        ip = modem.get_local_ip(5000);
-        printf("IP: %s\n", ip.c_str());
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-    } while (ip == "");
+    // String ip;
+    // do
+    // {
+    //     ip = modem.get_local_ip(5000).toString();
+    //     printf("IP: %s\n", ip.c_str());
+    //     vTaskDelay(2000 / portTICK_PERIOD_MS);
+    // } while (ip == "");
 
-    time_t time;
-    do
-    {
-        time = modem.get_ntp_time(5000);
-        printf("TIME: %ld\n", time);
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-    } while (time == 0);
+    // time_t time;
+    // do
+    // {
+    //     time = modem.get_ntp_time(5000);
+    //     printf("TIME: %ld\n", time);
+    //     vTaskDelay(2000 / portTICK_PERIOD_MS);
+    // } while (time == 0);
 
-    bool sub = false;
-    bool pub = false;
-    bool dis = false;
+    // bool sub = false;
+    // bool pub = false;
+    // bool dis = false;
+
+    // byte payload[3] = "OK";
 
 #ifndef SSL
     connected = modem.mqtt_connect("test.mosquitto.org", 1883, "A7672SA");
@@ -127,7 +132,7 @@ start:
         sub = modem.mqtt_subscribe_topics(topics, 10, 0, 5000);
         ESP_LOGI("MQTT_SUBSCRIBE TOPICS -> ", "%d", sub);
 
-        modem.mqtt_publish("teste/pub", "OK", strlen("OK"), 0, 5000);
+        modem.mqtt_publish("teste/pub", payload, 3, 0, 5000);
         ESP_LOGI("MQTT_PUBLISH -> ", "%d", pub);
 
         vTaskDelay(30000 / portTICK_PERIOD_MS);
@@ -136,24 +141,29 @@ start:
         ESP_LOGI("MQTT_DISCONNECT -> ", "%d", dis);
     }
 #else
-    bool cert = modem.set_ca_cert(ca_cert, "ca.pem", sizeof(ca_cert), 5000);
+
+    vTaskDelay(15000 / portTICK_PERIOD_MS);
+
+    // bool cert = modem.set_ca_cert(mqtt_ca_cert, "ca.pem", sizeof(mqtt_ca_cert), 5000);
+    bool cert = modem.set_ca_cert(https_ca_cert, "ca.pem", sizeof(https_ca_cert), 5000);
+
     ESP_LOGI("CERT", "%d", cert);
 
-    modem.mqtt_connect("test.mosquitto.org", 8883, "A7672SA", true, "", "", true, "ca.pem");
-    ESP_LOGI("MQTT_CONNECTED_SSL -> ", "%d", connected);
-    if (connected)
-    {
-        sub = modem.mqtt_subscribe("teste/sub", 0, 5000);
-        ESP_LOGI("MQTT_SUBSCRIBE -> ", "%d", sub);
+    // modem.mqtt_connect("test.mosquitto.org", 8883, "A7672SA", true, "", "", true, "ca.pem");
+    // ESP_LOGI("MQTT_CONNECTED_SSL -> ", "%d", connected);
+    // if (connected)
+    // {
+    //     sub = modem.mqtt_subscribe("teste/sub", 0, 5000);
+    //     ESP_LOGI("MQTT_SUBSCRIBE -> ", "%d", sub);
 
-        pub = modem.mqtt_publish("teste/pub", "OK", strlen("OK"), 0, 5000);
-        ESP_LOGI("MQTT_PUBLISH -> ", "%d", pub);
+    //     pub = modem.mqtt_publish("teste/pub", payload, 3, 0, 5000);
+    //     ESP_LOGI("MQTT_PUBLISH -> ", "%d", pub);
 
-        vTaskDelay(60000 / portTICK_PERIOD_MS);
+    //     vTaskDelay(60000 / portTICK_PERIOD_MS);
 
-        dis = modem.mqtt_disconnect(5000);
-        ESP_LOGI("MQTT_DISCONNECT -> ", "%d", dis);
-    }
+    //     dis = modem.mqtt_disconnect(5000);
+    //     ESP_LOGI("MQTT_DISCONNECT -> ", "%d", dis);
+    // }
 #endif
 
     ESP_LOGI("SETUP", "END");
@@ -161,5 +171,23 @@ start:
 
 void loop()
 {
-    yield();
+    int len = 0;
+    // vTaskDelay(60000 / portTICK_PERIOD_MS);
+    ESP_LOGI("HTTP_REQUEST RESPONSE LEN", "%d", len);
+    modem.stop();
+    if (len > 0)
+    {
+        const int size = strlen("AT+HTTPREAD=0,1024" GSM_NL);
+        const int txBytes = uart_write_bytes(UART_NUM_1, "AT+HTTPREAD=0,1024" GSM_NL, size);
+        // modem.http_read_response(1024);
+        // read by chunks
+        // char data[1024];
+    }
+    //     modem.http_read_file("hook.dat");
+    // vTaskDelay(60000 / portTICK_PERIOD_MS);
+    // modem.http_term();
+    // vTaskDelay(15000 / portTICK_PERIOD_MS);
+    // modem.read_file("https_body.dat", 1364512);
+    vTaskDelay(60000 / portTICK_PERIOD_MS);
+    ESP_LOGI("LOOP", "END");
 }
